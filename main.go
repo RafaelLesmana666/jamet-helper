3268c8b6a639fdb32b558f442fc82a659d134bcb
Lawliet <rafaellesmana666@gmail.com>
2025-02-26 16:11:36 +0700

    new feature redis

--- main.go
+++ main.go
@@ -1,83 +1,148 @@
 package jamethelper
 
 import (
+	"context"
+	"crypto/md5"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
 	"strings"
-	"encoding/json"
-	"log"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/render"
 	"github.com/go-sql-driver/mysql"
 	"github.com/google/uuid"
+	"github.com/redis/go-redis/v9"
 	"gorm.io/gorm"
 )
 
-
 type Jamet struct {
-	config map[string]*gorm.DB
+	Config map[string]*gorm.DB
+	Redis  FormatRedis
 }
 
-func NewJamet(config map[string]*gorm.DB) *Jamet {
+type FormatRedis struct {
+	Host, Port, Password string
+	Database             int
+	On                   bool
+}
+
+func NewJamet(param Jamet) *Jamet {
 	return &Jamet{
-		config:     config,
+		Config: param.Config,
+		Redis:  param.Redis,
 	}
 }
 
 func (met Jamet) GetData(table string, connection string) *gorm.DB {
 
-	return met.config[connection].Table(table)
+	return met.Config[connection].Table(table)
 }
 
 func (met Jamet) Connection(conn string) *gorm.DB {
-	db := met.config[conn]
+	db := met.Config[conn]
 
 	return db.Begin()
 }
 
-func (met Jamet) SinchronizeID(id string, char string, format int32) string {
+func (met Jamet) SinchronizeID(db *gorm.DB, id string, char string, format int32) string {
 	defer ErrorLog()
 
 	var getMstRunNum map[string]interface{}
-	query := met.config[""]
-
-	db := query
-	db.Table("mst_run_nums").Where(map[string]interface{}{"val_id": id,"val_char": char}).Find(&getMstRunNum)
+	db.Table("mst_run_nums").Where(map[string]interface{}{"val_id": id, "val_char": char}).Find(&getMstRunNum)
 
 	var value string
 	if len(getMstRunNum) != 0 {
 
-		num, err := strconv.Atoi(getMstRunNum["val_value"].(string)); 
+		num, err := strconv.Atoi(getMstRunNum["val_value"].(string))
 		if err != nil {
 			panic(err)
 		}
 
-		num = num+1;
-		db.Table("mst_run_nums").Where(map[string]interface{}{"val_id": id,"val_char": char}).Updates(map[string]interface{}{"val_value":num})
+		num = num + 1
+		db.Table("mst_run_nums").Where(map[string]interface{}{"val_id": id, "val_char": char}).Updates(map[string]interface{}{"val_value": num})
 
 		value = fmt.Sprintf("%0*d", format, num)
 	} else {
 		value = fmt.Sprintf("%0*d", format, 1)
-		InsertData(query, "mst_run_nums", map[string]interface{}{
-			"id": UUID(),
+		InsertData(db, "mst_run_nums", map[string]interface{}{
+			"id":        UUID(),
 			"val_value": 1,
-			"val_id": id,
-			"val_char": char,
+			"val_id":    id,
+			"val_char":  char,
 		})
 	}
 
-	return fmt.Sprintf("%s%s%s",id,char,value)
+	return fmt.Sprintf("%s%s%s", id, char, value)
 }
 
+func (met Jamet) ReadCache(previx string) (bool, Response) {
 
+	ctx := context.Background()
+
+	format := met.Redis
+
+	if format.On {
+		client := redis.NewClient(&redis.Options{
+			Addr:     fmt.Sprintf("%s:%s", format.Host, format.Port),
+			Password: format.Password, // No password set
+			DB:       format.Database, // Use default DB
+		})
+
+		val, err := client.Get(ctx, previx).Result()
+		if err != nil {
+			return false, Response{}
+		}
+
+		convert := Converter(val)
+
+		return true, Response{
+			Status: convert["status"].(bool),
+			Data:   convert["data"],
+		}
+
+	} else {
+		return false, Response{}
+	}
+}
+
+func (met Jamet) WriteCache(previx string, data any) {
+
+	ctx := context.Background()
+
+	format := met.Redis
+
+	if format.On {
+		client := redis.NewClient(&redis.Options{
+			Addr:     fmt.Sprintf("%s:%s", format.Host, format.Port),
+			Password: format.Password, // No password set
+			DB:       format.Database, // Use default DB
+		})
+
+		jsonStr, err := json.Marshal(data)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		res, err := client.Set(ctx, previx, jsonStr, 0).Result()
+		if err != nil {
+			panic(err)
+		}
+
+		log.Println(res)
+	}
+}
+
+// struct for response
 type Response struct {
 	Status  bool `json:"status"`
 	Message any  `json:"message"`
 	Data    any  `json:"data"`
 }
 
+// struct for datatable
 type DataTable struct {
 	Status          bool                     `json:"status"`
 	Draw            int                      `json:"draw"`
@@ -86,18 +151,22 @@ type DataTable struct {
 	RecordsTotal    int64                    `json:"recordsTotal"`
 }
 
+// get UUID
 func UUID() string {
 	return uuid.New().String()
 }
 
+// return JSON status true
 func PrintJSON(c *gin.Context, response Response) {
 	c.Render(http.StatusOK, render.JSON{Data: response})
 }
 
+// return JSON status false
 func EPrintJSON(c *gin.Context, response Response) {
 	c.Render(http.StatusBadRequest, render.JSON{Data: response})
 }
 
+// recover log after panic
 func ErrorLog() {
 	message := recover()
 
@@ -118,13 +187,12 @@ func ArrayKey(ar map[string]interface{}) []string {
 	return keys
 }
 
-func CreateData(c *gin.Context, table *gorm.DB, field []string) {
+func CreateData(c *gin.Context, table *gorm.DB, field []string) Response {
 
 	defer ErrorLog()
 
 	query := table
 	for _, value := range field {
-		fmt.Println(value)
 		check := c.Query(value)
 		if check != "" {
 			query.Where(value+" = ?", c.Query(value))
@@ -140,15 +208,13 @@ func CreateData(c *gin.Context, table *gorm.DB, field []string) {
 		query.Limit(limit).Find(&results)
 	}
 
-	fmt.Println(query);
-
-	PrintJSON(c, Response{
+	return Response{
 		Status: true,
 		Data:   results,
-	})
+	}
 }
 
-func CreateDataTable(c *gin.Context, table *gorm.DB, search []string) {
+func CreateDataTable(c *gin.Context, table *gorm.DB, search []string) Response {
 
 	defer ErrorLog()
 
@@ -216,13 +282,17 @@ func CreateDataTable(c *gin.Context, table *gorm.DB, search []string) {
 	var results []map[string]interface{}
 
 	query.Limit(limit).Offset(offset).Find(&results).Count(&recordsTotal)
-	c.Render(http.StatusOK, render.JSON{Data: DataTable{
-		Status:          true,
-		Draw:            draw,
-		Data:            results,
-		RecordsFiltered: recordsTotal,
-		RecordsTotal:    recordsTotal,
-	}})
+
+	return Response{
+		Status: true,
+		Data: DataTable{
+			Status:          true,
+			Draw:            draw,
+			Data:            results,
+			RecordsFiltered: recordsTotal,
+			RecordsTotal:    recordsTotal,
+		},
+	}
 }
 
 func InsertData(db *gorm.DB, table string, data any) any {
@@ -244,7 +314,7 @@ func InsertData(db *gorm.DB, table string, data any) any {
 
 func Contains(s []string, str string) (bool, string) {
 	for i, v := range s {
-		if strings.Contains(v,str) {
+		if strings.Contains(v, str) {
 			index := fmt.Sprintf("%d", i)
 			return true, index
 		}
@@ -256,9 +326,9 @@ func Validation(request map[string]interface{}, format map[string]map[string]str
 
 	var errMessage []string
 	message := format["message"]
-	alias   := format["alias"]
+	alias := format["alias"]
 	for key, value := range format["field"] {
-		
+
 		var contain bool
 		var index string
 		cond := strings.Split(value, "|")
@@ -266,14 +336,14 @@ func Validation(request map[string]interface{}, format map[string]map[string]str
 
 		contain, _ = Contains(cond, "required")
 		if contain && formData == "" {
-			
+
 			if len(message) > 0 && message[key] != "" {
-				errMessage = append(errMessage,message[key])
+				errMessage = append(errMessage, message[key])
 			} else {
 				if len(alias) > 0 && alias[key] != "" {
-					errMessage = append(errMessage,fmt.Sprintf("%s perlu diisi", alias[key]))
+					errMessage = append(errMessage, fmt.Sprintf("%s perlu diisi", alias[key]))
 				} else {
-					errMessage = append(errMessage,fmt.Sprintf("%s perlu diisi", key))
+					errMessage = append(errMessage, fmt.Sprintf("%s perlu diisi", key))
 				}
 			}
 
@@ -284,22 +354,24 @@ func Validation(request map[string]interface{}, format map[string]map[string]str
 		contain, index = Contains(cond, "min")
 		if contain {
 
-			i, err := strconv.Atoi(index); if err != nil {
-				errMessage = append(errMessage,"Error pada saat validasi")
-				break;
+			i, err := strconv.Atoi(index)
+			if err != nil {
+				errMessage = append(errMessage, "Error pada saat validasi")
+				break
 			}
 
-			arr := strings.Split(cond[i],":")
-			min, err := strconv.Atoi(arr[1]); if err != nil {
-				errMessage = append(errMessage,"Error pada saat validasi")
-				break;
+			arr := strings.Split(cond[i], ":")
+			min, err := strconv.Atoi(arr[1])
+			if err != nil {
+				errMessage = append(errMessage, "Error pada saat validasi")
+				break
 			}
 
 			if len(formData) < min {
 				if len(alias) > 0 && alias[key] != "" {
-					errMessage = append(errMessage,fmt.Sprintf("Panjang %s kurang dari %d", alias[key],min))
+					errMessage = append(errMessage, fmt.Sprintf("Panjang %s kurang dari %d", alias[key], min))
 				} else {
-					errMessage = append(errMessage,fmt.Sprintf("Panjang %s kurang dari %d", key,min))
+					errMessage = append(errMessage, fmt.Sprintf("Panjang %s kurang dari %d", key, min))
 				}
 				continue
 			}
@@ -309,22 +381,24 @@ func Validation(request map[string]interface{}, format map[string]map[string]str
 		contain, index = Contains(cond, "max")
 		if contain {
 
-			i, err := strconv.Atoi(index); if err != nil {
-				errMessage = append(errMessage,"Error pada saat validasi")
-				break;
+			i, err := strconv.Atoi(index)
+			if err != nil {
+				errMessage = append(errMessage, "Error pada saat validasi")
+				break
 			}
 
-			arr := strings.Split(cond[i],":")
-			max, err := strconv.Atoi(arr[1]); if err != nil {
-				errMessage = append(errMessage,"Error pada saat validasi")
-				break;
+			arr := strings.Split(cond[i], ":")
+			max, err := strconv.Atoi(arr[1])
+			if err != nil {
+				errMessage = append(errMessage, "Error pada saat validasi")
+				break
 			}
 
 			if len(formData) > max {
 				if len(alias) > 0 && alias[key] != "" {
-					errMessage = append(errMessage,fmt.Sprintf("Panjang %s lebih dari %d", alias[key],max))
+					errMessage = append(errMessage, fmt.Sprintf("Panjang %s lebih dari %d", alias[key], max))
 				} else {
-					errMessage = append(errMessage,fmt.Sprintf("Panjang %s lebih dari %d", key,max))
+					errMessage = append(errMessage, fmt.Sprintf("Panjang %s lebih dari %d", key, max))
 				}
 				continue
 			}
@@ -338,14 +412,36 @@ func Validation(request map[string]interface{}, format map[string]map[string]str
 	}
 }
 
-func Converter(c *gin.Context) map[string]interface{} {
+func GetRequest(c *gin.Context) []byte{
 
-	req := c.PostForm("data")
-	convert := fmt.Sprintf(`[%s]`,req)
+	param := c.Request.URL.Query()
+
+	mars, err := json.Marshal(param)
+	if err != nil {
+		panic(err)
+	}
+	// convert := fmt.Sprintf(`[%s]`, data)
+	// var con = []byte(convert)
+	body, err := c.GetRawData()
+	if err != nil {
+		panic(err)
+	}
+
+	return append(mars, body...);
+}
+
+func Md5(data []byte) string {
+
+	return fmt.Sprintf("%x", md5.Sum(data))
+}
+
+func Converter(req any) map[string]interface{} {
+
+	convert := fmt.Sprintf(`[%s]`, req)
 	var jsonBlob = []byte(convert)
 	var objmap []map[string]interface{}
 	if err := json.Unmarshal(jsonBlob, &objmap); err != nil {
-	    log.Fatal(err)
+		log.Fatal(err)
 	}
 
 	return objmap[0]
